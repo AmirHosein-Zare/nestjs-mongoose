3636062e9cd545ff6e85babdbb1e5c59512162c5
AmirHosein <awir.software@gmail.com>
2023-06-08 16:39:49 +0330

    set mongo module in user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schema/user.schema';
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}])],
     providers: [UserService],
     controllers: [UserController]
 })
