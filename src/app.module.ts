fd4048a6e7c9b6e65e699acc14d742fe8fe2e0ff
AmirHosein <awir.software@gmail.com>
2023-06-08 16:23:39 +0330

    add user set

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/demo')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/demo'), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
