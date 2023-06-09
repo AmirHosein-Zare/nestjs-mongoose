304d3ab4e4bf3398932fc465365317b83e575290
AmirHosein <awir.software@gmail.com>
2023-06-09 17:44:09 +0330

    finish user controler

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './schema/user.schema';
+import { FilterQuery } from 'mongoose';
 
 @Controller('user')
 export class UserController {
@@ -11,5 +12,30 @@ export class UserController {
     @Get()
     async findAll(): Promise<User[]>{
         return await this.userService.findAll();
+    }
+
+    @Get(':id')
+    async findById(@Param('id') id: string): Promise<User>{
+        return await this.userService.findById(id);
+    }
+
+    @Post('query')
+    async find(@Body() query: FilterQuery<User>): Promise<User[]>{
+        return await this.userService.findByQuery(query);
+    }
+
+    @Post()
+    async create(@Body() input: User): Promise<User>{
+        return await this.userService.create(input);
+    }
+
+    @Patch(':id')
+    async update(@Param('id') id: string, @Body() input: Partial<User>): Promise<User>{
+        return await this.userService.update(id, input);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: string): Promise<User>{
+        return await this.userService.delete(id);
     }
 }
