0064d98b04737dcf297a7f9106ec4cd068f16aae
AmirHosein <awir.software@gmail.com>
2023-06-09 17:07:39 +0330

    edit mongo url connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/demo'), UserModule],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/demo'), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
