05612c3217626927511225e71b93ce9b9718e381
AmirHosein <awir.software@gmail.com>
2023-06-09 17:02:46 +0330

    add findAll in userService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { User } from './schema/user.schema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        private readonly userRep: UserRepository
+    ){}
+
+    // find all
+    async findAll(): Promise<User[]>{
+        return await this.userRep.findAll();
+    }
+}
