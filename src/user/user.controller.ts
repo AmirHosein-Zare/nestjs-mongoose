8a203726e9d8086a2ad4327d5285cf6b0790b170
AmirHosein <awir.software@gmail.com>
2023-06-09 17:08:56 +0330

    add get all api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './schema/user.schema';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(
+        private readonly userService: UserService
+    ){}
+
+    @Get()
+    async findAll(): Promise<User[]>{
+        return await this.userService.findAll();
+    }
+}
