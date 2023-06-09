2764828707b1b9ee76b53152a314001f73e1bd28
AmirHosein <awir.software@gmail.com>
2023-06-09 17:27:16 +0330

    add findbyquery service method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { User } from './schema/user.schema';
+import { FilterQuery } from 'mongoose';
 
 @Injectable()
 export class UserService {
@@ -19,4 +20,13 @@ export class UserService {
         if(!user) throw new NotFoundException();
         return user;
     }
+
+    // find by query
+    async findByQuery(query: FilterQuery<User>):Promise<User[]>{
+        const user = await this.userRep.find(query);
+        if(!user) throw new NotFoundException();
+        return user;
+    }
+
+    
 }
