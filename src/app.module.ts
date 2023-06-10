6343a1bde65d4d016b973e7d5325051a9c4d488d
AmirHosein <awir.software@gmail.com>
2023-06-10 23:59:34 +0330

    add auth service & module & controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/demo'), UserModule],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/demo'), UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
