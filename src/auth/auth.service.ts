06708e46be9503d329ff3e1694036a474b97cb6d
AmirHosein <awir.software@gmail.com>
2023-06-11 00:47:21 +0330

    add local strategy and set authservice

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from "src/user/user.service";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly userService: UserService
+    ){}
+
+    async validate(username: string, password: string): Promise<any>{
+        const user = await this.userService.findByQuery({username: username});
+        if(user[0] && user[0].password === password){
+            return user[0];
+        }
+
+        return null;
+    }
+}
