8594cedf2949623e98827248d3a70893c4d69d06
AmirHosein <awir.software@gmail.com>
2023-06-11 00:50:14 +0330

    set user module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UserService } from 'src/user/user.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { UserModule } from "../user/user.module";
 
 @Module({
-  imports: [],
-  providers: [AuthService, UserService],
+  imports: [PassportModule, UserModule],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
