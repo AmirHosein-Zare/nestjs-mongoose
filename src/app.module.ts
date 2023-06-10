e12b31d215a2b6df64f3cb9af8fcd4fbe888f3a8
AmirHosein <awir.software@gmail.com>
2023-06-11 00:06:45 +0330

    set modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
-import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/demo'), UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, AuthService],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserService } from 'src/user/user.service';
 
 @Module({
+  imports: [],
+  providers: [AuthService, UserService],
   controllers: [AuthController]
 })
 export class AuthModule {}
