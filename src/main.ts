06348622faf606759e86ce42a21ee2b50cf3bb15
AmirHosein Zare <awir.software@gmail.com>
2023-06-11 21:58:58 +0330

    add passport middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as passport from 'passport';
+const session = require('express-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+  }));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   await app.listen(3000);
 }
 bootstrap();
