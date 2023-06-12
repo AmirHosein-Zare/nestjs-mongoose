453f19ed169ea554effcbb765c57d06787de53c5
AmirHosein Zare <awir.software@gmail.com>
2023-06-13 01:26:46 +0330

    add multer module

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+  imports:[
+    MulterModule.register({
+      dest: './upload'
+    })
+  ],
   providers: [UploadService],
   controllers: [UploadController]
 })
