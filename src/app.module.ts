df32186ca29e6713b51d9d15ebe49ef679c4da8c
AmirHosein Zare <awir.software@gmail.com>
2023-06-13 01:25:32 +0330

    add upload dir

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/demo'), UserModule, AuthModule],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/demo'), UserModule, AuthModule, UploadModule],
   controllers: [AppController],
   providers: [AppService],
 })
