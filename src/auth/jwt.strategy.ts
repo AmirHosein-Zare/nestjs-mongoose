e6fa0f6719ea440ddbd9866babb1371ad47b6312
AmirHosein Zare <awir.software@gmail.com>
2023-06-13 23:20:45 +0330

    set JwtStrategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,19 +1,22 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-jwt";
+import { ExtractJwt, Strategy } from "passport-jwt";
 import { AuthService } from "./auth.service";
+import { JwtConstant } from "src/config/jwt.constant";
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy){
+export class JwtStrategy extends PassportStrategy(Strategy){
     constructor(
-        
+        private readonly authService : AuthService
     ){
         super({
-            
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
+            ignoreExpiration: false,
+            secretOrKey: JwtConstant.secret
         });
     }
 
     async validate(payload: any): Promise<any>{
-        
+        return await this.authService.getProfile(payload);
     }
 }
